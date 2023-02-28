b86900b79a4bf718024420a6025b3e0aa3ffe422
BartG2 <126042480+BartG2@users.noreply.github.com>
2023-02-28 15:29:21 -0500

    Single thread collisions

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <omp.h>
 #include <array>
 #include <algorithm>
+#include <unordered_set>
 //#include <cuda_runtime.h>
 //#include <device_launch_parameters.h>
 
@@ -18,7 +19,7 @@ float RandomFloat(float min, float max, std::mt19937& rng);
 //---------------------------------------------------------------------------------------------------------------------------------
 
 
-const int screenWidth = 2560, screenHeight = 1440, numThreads = 20;
+const int screenWidth = 2560, screenHeight = 1440, numThreads = 24;
 int startingNumParticles = 20000, startingClusterParticles = 1;
 const float collisionThreshold = 3.5f;
 Vector2 particleSize = {2,2};
@@ -148,54 +149,6 @@ public:
         }
     }
 
-    /*std::vector<Particle> queryCircle(const Vector2& center, float radius) {
-        std::vector<Particle> results;
-
-        //std::cout << "Length 1: " << results.size() << std::endl;
-        
-        bool collides = CheckCollisionCircleRec(center, radius, boundary);
-        if (!collides) {
-            return results;
-        }
-
-        //std::cout << "Length 2: " << results.size() << std::endl;
-
-        for (long long unsigned int i = 0; i < particles.size(); i++) {
-            if(this->isLeaf()){
-                if (CheckCollisionPointCircle(particles[i].pos, center, radius)) {
-                    Particle p {particles[i].pos.x, particles[i].pos.y, YELLOW};
-                    results.push_back(p);
-                    //std::cout << "Particle added: " << particles[i].pos.x << ", " << particles[i].pos.y << std::endl;
-                }
-            }
-        }
-
-        //std::cout << "Length 3: " << results.size() << std::endl;
-
-        if (!children[0]) {
-            return results;
-        }
-
-        //std::cout << "Length 4: " << results.size() << std::endl;
-
-        for (int i = 0; i < 4; i++) {
-            auto child_results = children[i]->queryCircle(center, radius);
-            if (!child_results.empty()) {
-                results.reserve(results.size() + child_results.size());
-                results.insert(results.end(), std::make_move_iterator(child_results.begin()),
-                            std::make_move_iterator(child_results.end()));
-            }
-        }
-
-        //std::cout << "Length 5: " << results.size() << std::endl;
-
-        for(unsigned int i = 0; i < results.size(); i++){
-            results[i].color = BLUE;
-        }
-
-        return results;
-    }*/
-
     std::vector<Particle> queryCircle(const Vector2& center, float radius) {
         std::vector<Particle> results;
 
@@ -261,7 +214,7 @@ public:
             DrawPixelV(p.pos, p.color);
         }
 
-        DrawRectangleLinesEx(boundary, 1, GREEN);
+        //DrawRectangleLinesEx(boundary, 1, GREEN);
 
         if (isDivided) {
             for (auto& child : children) {
@@ -274,6 +227,10 @@ public:
         auto end = std::chrono::high_resolution_clock::now();
         std::chrono::duration<float> duration = end - start;
         return duration;
+    }
+
+    void eraseParticle(int j){
+        particles.erase(particles.begin() + j);
     }
 };
 
@@ -304,41 +261,6 @@ float vector2distance(Vector2 v1, Vector2 v2) {
     return std::sqrt(dx * dx + dy * dy);
 }
 
-/*__global__ void vector2distanceKernel(Vector2* v1, Vector2* v2, float* result)
-{
-    int idx = blockIdx.x * blockDim.x + threadIdx.x;
-    float dx = v2[idx].x - v1[idx].x;
-    float dy = v2[idx].y - v1[idx].y;
-    result[idx] = sqrtf(dx * dx + dy * dy);
-}*/
-
-/*void vector2distanceCUDA(std::vector<Vector2>& v1, std::vector<Vector2>& v2, std::vector<float>& result)
-{
-    const int size = v1.size();
-    Vector2* d_v1;
-    Vector2* d_v2;
-    float* d_result;
-
-    cudaMalloc((void**)&d_v1, size * sizeof(Vector2));
-    cudaMalloc((void**)&d_v2, size * sizeof(Vector2));
-    cudaMalloc((void**)&d_result, size * sizeof(float));
-
-    cudaMemcpy(d_v1, v1.data(), size * sizeof(Vector2), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_v2, v2.data(), size * sizeof(Vector2), cudaMemcpyHostToDevice);
-
-    const int threadsPerBlock = 256;
-    const int blocksPerGrid = (size + threadsPerBlock - 1) / threadsPerBlock;
-
-    vector2distanceKernel<<<blocksPerGrid, threadsPerBlock>>>(d_v1, d_v2, d_result);
-
-    result.resize(size);
-    cudaMemcpy(result.data(), d_result, size * sizeof(float), cudaMemcpyDeviceToHost);
-
-    cudaFree(d_v1);
-    cudaFree(d_v2);
-    cudaFree(d_result);
-}*/
-
 std::vector<Particle> CreateCircle(int numParticles, Color col, Vector2 center, float radius){
     float degreeIncrement = 360.0f/(float)numParticles;
     std::vector<Particle> particles;
@@ -353,56 +275,7 @@ std::vector<Particle> CreateCircle(int numParticles, Color col, Vector2 center, 
     return particles;
 }
 
-
-//---------------------------------------------------------------------------------------------------------------------------------
-
-int main() {
-
-    float queryRadius = 100;
-
-    InitWindow(screenWidth, screenHeight, "DLA");
-    SetTargetFPS(100);
-    omp_set_num_threads(20);
-
-    //std::vector<Particle> FreeParticles(startingNumParticles,Particle(1000,700,RED));
-    std::vector<Particle> FreeParticles = CreateCircle(1000,RED,{screenWidth/2.0,screenHeight/2.0}, queryRadius + 10);
-    //std::vector<Particle> FreeParticles(1000, Particle(RandomFloat(0,screenWidth, rng),RandomFloat(0,screenHeight, rng), RED));     //random particles
-    std::vector<Particle> ClusterParticles(startingClusterParticles,Particle(screenWidth/2.0,screenHeight/2.0,WHITE));
-
-    Camera2D camera = { 0 };
-    camera.target = { screenWidth / 2.0f, screenHeight / 2.0f };
-    camera.offset = { screenWidth / 2.0f, screenHeight / 2.0f };
-    camera.rotation = 0.0f;
-    camera.zoom = 1.0f;
-
-    QuadTree qt(Rectangle{0,0,screenWidth,screenHeight});
-
-    //main loop
-    for (int i = 0; !WindowShouldClose(); i++) {
-
-        auto startMainLoop = std::chrono::high_resolution_clock::now();
-
-        /*#pragma omp parallel for
-        for(long long unsigned int i = 0; i < FreeParticles.size(); i++){
-            FreeParticles[i].RandomWalk(1,2);
-        }*/
-
-        /*#pragma omp parallel for
-        for (long long unsigned int i = 0; i < ClusterParticles.size(); i++){
-            for(long long unsigned int j = 0; j < FreeParticles.size(); j++){
-                float distance = vector2distance(ClusterParticles[i].pos, FreeParticles[j].pos);
-
-                if (distance < collisionThreshold) {
-                    FreeParticles[j].isStuck = true;
-                    FreeParticles[j].color = WHITE;
-                    ClusterParticles.push_back(FreeParticles[j]);
-                    FreeParticles.erase(FreeParticles.begin() + j);
-                    break;
-                }
-            }
-        }*/
-
-        auto startVectorSplit = std::chrono::high_resolution_clock::now();
+std::vector<Particle> randomWalkMultiThreaded(std::vector<Particle> FreeParticles){
         // Split the FreeParticles vector into smaller chunks
         int chunkSize = FreeParticles.size() / numThreads;
         std::vector<std::vector<Particle>> particleChunks(numThreads);
@@ -422,7 +295,7 @@ int main() {
             newParticles.reserve(particles.size());
             for (const auto& particle : particles) {
                 Particle newParticle = particle;
-                newParticle.RandomWalk(1, 2);
+                newParticle.RandomWalk(1, 1);
                 newParticles.push_back(newParticle);
             }
             return newParticles;
@@ -436,36 +309,107 @@ int main() {
             newParticles.insert(newParticles.end(), result.begin(), result.end());
         }
 
-        auto endVectorSplit = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<float> vectorSplitDuration = endVectorSplit - startVectorSplit;
+        return newParticles;
+}
 
-        auto startVectorCopy = std::chrono::high_resolution_clock::now();
-        // Replace the old FreeParticles vector with the new one
-        FreeParticles = newParticles;
-        auto endVectorCopy = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<float> vectorCopyDuration = endVectorCopy - startVectorCopy;
 
-        auto startQT = std::chrono::high_resolution_clock::now();
-        //QuadTree qt(Rectangle{0,0,screenWidth,screenHeight});
+std::vector<Particle> checkCollisionsSingleThread(std::vector<Particle>& cluster, std::vector<Particle>& FreeParticles, QuadTree& qt){
+        std::vector<Particle> newParticles, result;
+
+        for(auto it = cluster.begin(); it != cluster.end(); ++it){
+            newParticles = {qt.queryCircle(it->pos, collisionThreshold)};
+            for(auto& p : newParticles){
+                if(CheckCollisionPointCircle(it->pos, p.pos, collisionThreshold / 1.5f)){
+                    result.push_back(p);
+                    for(auto zt = FreeParticles.begin(); zt != FreeParticles.end(); ++zt){
+                        if(zt->pos.x == p.pos.x and zt->pos.y == p.pos.y){
+                            FreeParticles.erase(zt);
+                            break;  // exit the inner loop after erasing the particle
+                        }
+                    }
+                }
+            }
+        }
+        return result;   
+}
+
+
+std::vector<Particle> checkCollisions(std::vector<Particle>& cluster, std::vector<Particle>& FreeParticles, QuadTree& qt){
+    if(false){
+        // Split the cluster vector into smaller chunks
+        int chunkSize = cluster.size() / numThreads;
+        std::vector<std::vector<Particle>> particleChunks(numThreads);
+
+        for (int i = 0; i < numThreads; i++) {
+            int startIndex = i * chunkSize;
+            int endIndex = (i == numThreads - 1) ? cluster.size() : (i + 1) * chunkSize;
+            particleChunks[i] = std::vector<Particle>(cluster.begin() + startIndex, cluster.begin() + endIndex);
+        }
+
+        for(int i = 0; i < numThreads; i++){
+            //std::async(std::launch::async, checkCollisionsSingleThread(particleChunks[i], FreeParticles, qt));
+        }
+
+
+    }
+    else{
+        return checkCollisionsSingleThread(cluster, FreeParticles, qt);  
+    }
+}
+
+
+//---------------------------------------------------------------------------------------------------------------------------------
+
+int main() {
+
+    float queryRadius = 400;
+
+    InitWindow(screenWidth, screenHeight, "DLA");
+    SetTargetFPS(100);
+    omp_set_num_threads(20);
+
+    //std::vector<Particle> FreeParticles(startingNumParticles,Particle(1000,700,RED));
+    std::vector<Particle> FreeParticles = CreateCircle(20000,RED,{screenWidth/2.0,screenHeight/2.0}, queryRadius);
+    //std::vector<Particle> FreeParticles(1000, Particle(RandomFloat(0,screenWidth, rng),RandomFloat(0,screenHeight, rng), RED));     //random particles
+    std::vector<Particle> ClusterParticles(startingClusterParticles,Particle(screenWidth/2.0,screenHeight/2.0,GREEN));
+
+    Camera2D camera = { 0 };
+    camera.target = { screenWidth / 2.0f, screenHeight / 2.0f };
+    camera.offset = { screenWidth / 2.0f, screenHeight / 2.0f };
+    camera.rotation = 0.0f;
+    camera.zoom = 1.0f;
+
+    QuadTree qt(Rectangle{0,0,screenWidth,screenHeight});
+
+    //main loop
+    for (int i = 0; !WindowShouldClose(); i++) {
+
+        FreeParticles = randomWalkMultiThreaded(FreeParticles);
+
+        //clear quadtree
         qt.clear();
-        auto endQT = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<float> qtDuration = endQT - startQT;
 
-        auto start = std::chrono::high_resolution_clock::now();
+        //rebuilt quadtree
         for(long long unsigned int i = 0; i < FreeParticles.size(); i++){
-            //const Particle& p = FreeParticles[i];
-            //Particle *p = &FreeParticles[i];
             qt.Insert(FreeParticles[i]);
         }
-        auto end = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<float> insertDuration = end - start;
 
-        std::vector<Particle> centralPoints {qt.queryCircle({screenWidth/2.0,screenHeight/2.0}, queryRadius)};
+        //check for points in central circle using qt
+        //std::vector<Particle> centralPoints {qt.queryCircle({screenWidth/2.0,screenHeight/2.0}, queryRadius)};
 
-        for(unsigned int i = 0; i < centralPoints.size(); i++){
+        //make centralPoints green
+        /*for(unsigned int i = 0; i < centralPoints.size(); i++){
             Particle p = centralPoints[i];
             p.color = GREEN;
             centralPoints[i] = p; 
+        }*/
+
+
+        std::vector<Particle> collided {checkCollisions(ClusterParticles, FreeParticles, qt)};
+
+        for(auto p: collided){
+            p.color = PURPLE;
+            ClusterParticles.push_back(p);
         }
 
 
@@ -474,36 +418,29 @@ int main() {
         ClearBackground(BLACK);
         DrawFPS(20, 20);
 
-        /*//#pragma omp parallel for
-        for (long long unsigned int i = 0; i < FreeParticles.size(); i++) {
-            DrawRectangleV(FreeParticles[i].pos, { 2,2 }, FreeParticles[i].color);
-        }*/
-        //#pragma omp parallel for
+        //draw cluster
         for (long long unsigned int i = 0; i < ClusterParticles.size(); i++) {
             //DrawRectangleV(ClusterParticles[i].pos, { 2,2 }, ClusterParticles[i].color);
             DrawPixelV(ClusterParticles[i].pos,ClusterParticles[i].color);
         }
+
+        //draw quadtree + freepoints
         std::chrono::duration<float> drawDuration = qt.Draw();
 
-        DrawCircleLines(screenWidth/2.0, screenHeight/2.0, queryRadius, ORANGE);
+        //draw center circle
+        //DrawCircleLines(screenWidth/2.0, screenHeight/2.0, queryRadius, ORANGE);
 
-        for(long long unsigned int i = 0; i < centralPoints.size(); i++){
-            //DrawCircleV(centralPoints[i].pos,2,centralPoints[i].color);
+        //draw centralPoints
+        /*for(long long unsigned int i = 0; i < centralPoints.size(); i++){
             DrawPixelV(centralPoints[i].pos,centralPoints[i].color);
-        }
-
-
+        }*/
 
         EndDrawing();
 
-        auto endMainLoop = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<float> mainLoopDuration = endMainLoop - startMainLoop;
-
         if(i % 2000 == 0){
-            qt.debugPrint();
+            //qt.debugPrint();
         }
 
-        //std::cout << "Main Loop: " << mainLoopDuration.count() << "\tInsert: " << insertDuration.count()/mainLoopDuration.count() << "\tDraw: " << drawDuration.count()/mainLoopDuration.count() << "\tQT: " << std::fixed << qtDuration.count()/mainLoopDuration.count() << "\tVectorCopy: " << vectorCopyDuration.count()/mainLoopDuration.count() << "\tVectorSplit: " << vectorSplitDuration.count()/mainLoopDuration.count() << "\tRemainder: " << 1.0 - vectorSplitDuration.count()/mainLoopDuration.count() - vectorCopyDuration.count()/mainLoopDuration.count() - qtDuration.count()/mainLoopDuration.count() - drawDuration.count()/mainLoopDuration.count() - insertDuration.count()/mainLoopDuration.count() << std::endl;
     }
 
     CloseWindow();
